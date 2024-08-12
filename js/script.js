56840779ee89629a55a126627b9dcc40a20c83d1
yesenia_v <yeseniavsqz@gmail.com>
2024-08-11 17:45:43 -0700

    Added an if and else statements with conditions to see if an image of a cat appears when its clicked

--- js/script.js
+++ js/script.js
@@ -4,6 +4,15 @@ var cat = document.querySelector(".cat");
 //console.log(cat.outerHTML);
 
 button.addEventListener("click", function() {
-    button.innerText = "It's working";
-    cat.classList.add("show");
+    if (cat.classList.contains("show")) {
+        console.log("yes");
+        cat.classList.remove("show")
+        button.innerText = "Wait, come back!";
+        button.classList.add("disappear");
+} else {
+    console.log("No");
+    cat.classList.add("show")
+    button.innerText = "Shoo, cat";
+    button.classList.remove("disappear");
+}
 });
